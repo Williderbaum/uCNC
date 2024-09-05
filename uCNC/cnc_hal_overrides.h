ba10f84079b921d5935035455321687ab4870d86
Williderbaum <59510119+Williderbaum@users.noreply.github.com>
2024-09-05 15:02:11 +0200

    custom config added

--- uCNC/cnc_hal_overrides.h
+++ uCNC/cnc_hal_overrides.h
@@ -1,15 +1,29 @@
-// boardmap override dummy file
-
 #ifndef CNC_HAL_OVERRIDES_H
 #define CNC_HAL_OVERRIDES_H
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+#include "cnc_hal_reset.h"
+#define ENABLE_COOLANT
+#define S_CURVE_ACCELERATION_LEVEL 0
+#define BRESENHAM_16BIT
+#define ESTOP_PULLUP_ENABLE
+#define LIMIT_X_PULLUP_ENABLE
+#define LIMIT_Y_PULLUP_ENABLE
+#define LIMIT_X2_PULLUP_ENABLE
+#define LIMIT_Y2_PULLUP_ENABLE
+#define DISABLE_PROBE
+#define TOOL1 laser_pwm
+#define LASER_PWM PWM0
+#define LASER_FREQ 8000
+#define LASER_PWM_AIR_ASSIST DOUT2
+#define ENCODERS 0
+//Custom configurations
+
+
 #ifdef __cplusplus
 }
 #endif
-
 #endif
